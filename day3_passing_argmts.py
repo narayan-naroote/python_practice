0066895820d6c4f3f0eb81a8777ed25f67d7542a
NARAYANNAROOTE <narayannaroote@gmail.com>
2026-03-27 23:18:10 +0530

    Update day3_passing_argmts.py

--- day3_passing_argmts.py
+++ day3_passing_argmts.py
@@ -1,4 +1,16 @@
+1)
 #passing n number of values for adding
 def add(*numbers): #star or asterik
 	return sum(numbers)
 print(add(1,100,1))
+
+
+2)
+
+def  total_sum(*numbers):
+	result = 0
+	for num in numbers:
+		result+=num
+	return result
+print(total_sum(24,5,8))
+
