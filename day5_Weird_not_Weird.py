9dc9460874cb6a5bf0a43e0e145c563c7758726c
NARAYANNAROOTE <narayannaroote@gmail.com>
2026-03-29 13:12:21 +0530

    Create day5_Weird_not_Weird.py

---
+++ day5_Weird_not_Weird.py
@@ -0,0 +1,10 @@
+n = int(input("enter number:"))
+
+if n%2==1:       #checks odd
+	print("Weird")  #range or interval 
+elif 2<=n<=5:
+	print("Not weird")
+elif 6<=n<=20:
+	print("Weird")
+elif n>20:
+	print("Not weird")
