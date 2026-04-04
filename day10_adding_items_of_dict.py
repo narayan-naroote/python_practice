3b5f9c8dcf4cfe4c331ebbdcea742285938d823e
NARAYANNAROOTE <narayannaroote@gmail.com>
2026-04-04 13:58:57 +0530

    Create day10_adding_items_of_dict.py

---
+++ day10_adding_items_of_dict.py
@@ -0,0 +1,14 @@
+item1 = {
+"name" : "sugar" ,
+"price" : "23 rs",
+"weight" : 4
+}
+
+item2 = {
+"name" : "milk",
+"price" : "44 rs",
+"weight" : 5
+}
+items = [item1, item2]  #putting item1 and item2 in list
+
+print(f"Total weight: {item1["weight"] + item2["weight"]}kg") #adding the weight of the product from two items
