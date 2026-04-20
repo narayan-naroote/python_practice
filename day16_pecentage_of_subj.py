dbef477873a339a3f4f7620812c6b12d84d250e1
NARAYANNAROOTE <narayannaroote@gmail.com>
2026-04-20 22:57:20 +0530

    Create day16_pecentage_of_subj.py

---
+++ day16_pecentage_of_subj.py
@@ -0,0 +1,17 @@
+class Students:
+    def __init__(self, phy, chem, math):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
+
+    def percentage(self):
+        return round((self.phy + self.chem + self.math) / 3, 2)
+
+s1 = Students(89, 67, 56)
+
+print("Old %:", s1.percentage())
+
+# 🔹 Change marks
+s1.math = 90
+
+print("New %:", s1.percentage())
