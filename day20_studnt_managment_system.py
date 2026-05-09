0a12e37da8896c8607cc0c9a791bfa5929f3a86a
NARAYANNAROOTE <narayannaroote@gmail.com>
2026-05-09 16:31:17 +0530

    Update day20_studnt_managment_system.py

--- day20_studnt_managment_system.py
+++ day20_studnt_managment_system.py
@@ -1,4 +1,3 @@
-print("#####STUDENT MANAGEMENT SYSTEM####")
 def student_details():
     print("1. Add student details")
     print("2. Display student details")
@@ -13,6 +12,7 @@ while True:
     choice = int(input("Enter choice: "))
 
     if choice == 1:
+
         name = input("Enter student name: ")
         marks = int(input("Enter student marks: "))
         roll_no = int(input("Enter student roll number: "))
@@ -27,12 +27,14 @@ while True:
             print("No student details found\n")
 
         else:
-            print("\nSTUDENT DETAILS")
+            print("\nSTUDENT DETAILS\n")
 
             for student in details:
-                print(student)
 
-            print()
+                print("Name:", student[0])
+                print("Marks:", student[1])
+                print("Roll No:", student[2])
+                print("-------------------")
 
     elif choice == 3:
         print("You exited the program")
